806be703ee256f1d3c44ab693902503c91ad970b
watsonzr <watsonzr@rose-hulman.edu>
2018-03-25 10:42:53 -0400

    Still some spaces remaining in some print statements.

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -39,7 +39,7 @@ def main():
 
 
 # ----------------------------------------------------------------------
-# TODO: 2. Implement a class called   Baby   that has a constructor and
+# DONE: 2. Implement a class called   Baby   that has a constructor and
 #   two methods, as described below.  Your finished Baby class should
 #   cause the code above to display the expected output.  Hint: Your
 #   class will need instance variables that you must figure out.
@@ -90,10 +90,27 @@ def main():
 #
 ########################################################################
 
+class Baby(object):
 
+    def __init__(self, name):
+        self.name = name
+        print('Hello baby', name, '!')
+        self.hours = 0
 
+    def feed_baby(self):
+        print('Thank you for feeding baby', self.name, '.')
+        self.hours = 0
 
-
+    def hour_passes(self):
+        if self.hours == 0:
+            print('Baby', self.name, 'is sleeping.')
+            self.hours = self.hours + 1
+        elif self.hours == 1:
+            print('Baby', self.name, 'is awake.  Time for food.')
+            self.hours = self.hours + 1
+        else:
+            print('Baby', self.name, 'is CRYING uncontrollably! Feed the Baby!')
+            self.hours = self.hours + 1
 
 
 # ----------------------------------------------------------------------
